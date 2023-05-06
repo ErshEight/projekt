13816f9f88f3735bf9b30b9a189c2de9e90413ad
ErshEight <ersheight@gmail.com>
2023-05-06 15:01:50 +0200

    add activity

---
+++ app/src/main/java/com/example/projekt/successScreen.java
@@ -0,0 +1,14 @@
+package com.example.projekt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class successScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_success_screen);
+    }
+}
