645edd862ec58942d18b85585cb95d0320fd7ca3
ErshEight <ersheight@gmail.com>
2023-05-07 18:07:46 +0200

    fragment base

---
+++ app/src/main/java/com/example/projekt/fragments/DetailsFragment.java
@@ -0,0 +1,21 @@
+package com.example.projekt.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.projekt.R;
+
+public class DetailsFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_details, container, false);
+        return view;
+    }
+}
